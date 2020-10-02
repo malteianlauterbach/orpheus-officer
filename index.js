ca7e2f5f8cb7c6bccd39e5575aad81b3669074de
lukec11 <me@lukec.me>
2020-10-02 15:42:41 -0400

    fix for listening incorrectly

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.command('/report-form', async ({ ack, say }) => {
 
 (async () => {
   // Listen on port 3000
-  await app.listen(3000);
+  await app.start(3000);
   console.log('Listening on port 3000');
 })
